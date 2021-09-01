e56216927192a52248e1a82515f993a591af2843
wujianfeng7 <wujf@cndatacom.com>
2021-09-01 23:40:27 +0800

    react基础，from表单

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ShoppingList from './ShoppingList';
 import Count from './count';
+import From from './from';
 
 function App() {
   return (
     <div className="App">
       <ShoppingList Mark="Mark" />
       <Count />
+      <From />
     </div>
   );
 }
---
+++ React/my-app/src/from.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class From extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="from">
+        from表单
+      </div>
+    )
+  }
+}
+export default From
