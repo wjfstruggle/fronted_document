d3b4fe7584da97fca29e731676082863922a0dc8
wujianfeng7 <wujf@cndatacom.com>
2022-02-10 17:54:22 +0800

    JS

--- 高级JS/38_高级JS整理/17原型链实现继承.js
+++ 高级JS/38_高级JS整理/17原型链实现继承.js
@@ -1,27 +1,33 @@
-// 定义父类构造函数
+//1、 定义父类构造函数
 function Person() {
   this.name = "why"
+  this.friends = []
 }
 
-// 父类原型上添加方法
+//2、 父类原型上添加方法
 Person.prototype.running = function() {
   console.log(`${this.name}: 在running`);
 }
 
-// 定义子类构造函数
+//3、 定义子类构造函数
 function Student() {
-  this.age = 18
+  this.sno = 111
 }
 
-// 创建父类p对象
+//4、 创建父类p对象
 
 var p = new Person();
 Student.prototype = p;
 
+// 5、子类原型上添加方法
 Student.prototype.studying = function() {
   console.log(`${this.name}: 在studying`);
 }
 
 var stu = new Student();
 stu.studying()
-console.log(stu); // Person { age: 18 }
+console.log(stu); // Person { sno: 111 }
+// 1、第一个弊端：继承的属性是看不到的
+// 2、第二个弊端
+var stu1 = new Student("why", 18, ["lilei"], 111)
+var stu2 = new Student("kobe", 30, ["james"], 112)
---
+++ 高级JS/38_高级JS整理/18解构赋值.js
@@ -0,0 +1,6 @@
+let {obj1, obj2} = {obj1:"obj1对象",obj2:"obj2对象"}
+console.log(obj1,obj2);
+
+let names = "wjf"
+console.log("你的名字：" + names)
+console.log(`你的名字：${names}`)
